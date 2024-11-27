1c8fb73e2dea5a5d33a4ca16364c611716608632
Nitin Gatla <gatlanit@gmail.com>
2024-11-26 22:44:48 -0600

    Cleaned up code surrounding randomize BPM function for future usage

--- src/main.c
+++ src/main.c
@@ -1,14 +1,15 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 /*
   TODO:
-  Randomize Key
-  Randomize Mode
-  Randomize Chord progression (depending
-  on chord progresion length that is
-  also random)
+  - Randomize Key
+  - Randomize Mode
+  - Randomize Chord progression
+      - depending on chord progresion length that is also random
 */
 
 int generate_bpm() {
@@ -17,8 +18,20 @@ int generate_bpm() {
 }
 
 int main() {
+  char input[10]; // Buffer for user input
 
-  printf("BPM: %d", generate_bpm());
+  do {
+    // Check if input is "q", "Quit", or "quit"
+    if (strcmp(input, "q") == 0 || strcmp(input, "Quit") == 0 ||
+        strcmp(input, "quit") == 0) {
+      break; // Exit the loop if the user enters "q", "Quit", or "quit"
+    }
 
+    printf("BPM: %d\n", generate_bpm());
+    printf("\nRun again? Type anything to refresh (or \"q\" to exit): ");
+    scanf("%9s", input); // Read input from the user (up to 9 characters)
+  } while (1); // Infinite loop controlled by `break`
+
+  printf("Exiting song starter...\n");
   return 0;
 }
