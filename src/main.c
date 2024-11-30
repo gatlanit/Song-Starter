a77763c1e35b8c17a310d0c1c0234c847a139ce9
Nitin Gatla <gatlanit@gmail.com>
2024-11-29 21:16:06 -0600

    Added randomize function for keys and a couple modes (Major and Minor)

--- src/main.c
+++ src/main.c
@@ -6,8 +6,6 @@
 
 /*
   TODO:
-  - Randomize Key
-  - Randomize Mode
   - Randomize Chord progression
       - depending on chord progresion length that is also random
 */
@@ -17,20 +15,38 @@ int generate_bpm() {
   return rand() % (200 - 75 + 1) + 75;
 }
 
+char *generate_key() {
+
+  static const char *keys[] = {"C",  "C♯", "D",  "D♯", "E",  "F",
+                               "F♯", "G",  "G♯", "A",  "A♯", "B"};
+
+  return (char *)keys[rand() % (11 - 0 + 1) + 0];
+}
+
+char *generate_mode() {
+
+  static const char *modes[] = {"Major", "Minor"};
+
+  return (char *)modes[rand() % (1 - 0 + 1) + 0];
+}
+
 int main() {
   char input[10]; // Buffer for user input
 
   do {
-    // Check if input is "q", "Quit", or "quit"
-    if (strcmp(input, "q") == 0 || strcmp(input, "Quit") == 0 ||
-        strcmp(input, "quit") == 0) {
+    // Check if input is "q", "Q", "Quit", or "quit"
+    if (strcmp(input, "q") == 0 || strcmp(input, "Q") == 0 ||
+        strcmp(input, "Quit") == 0 || strcmp(input, "quit") == 0) {
       break; // Exit the loop if the user enters "q", "Quit", or "quit"
     }
 
     printf("BPM: %d\n", generate_bpm());
+    printf("Key: %s\n", generate_key());
+    printf("Modes: %s\n", generate_mode());
+
     printf("\nRun again? Type anything to refresh (or \"q\" to exit): ");
-    scanf("%9s", input); // Read input from the user (up to 9 characters)
-  } while (1); // Infinite loop controlled by `break`
+    scanf("%9s", input);
+  } while (1);
 
   printf("Exiting song starter...\n");
   return 0;
